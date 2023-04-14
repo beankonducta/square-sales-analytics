4548fb0c0db91a1b8bdb34d98ab9882831cf39ce
patrick andrews <patrickxandrews@gmail.com>
2023-04-13 20:07:10 -0600

    add pagination to orders endpoint

--- square-sales-server/functions/index.js
+++ square-sales-server/functions/index.js
@@ -42,42 +42,49 @@ exports.timecards = functions.https.onRequest(async (req, res) => {
 // Returns an object of sales data, based on provided date range and location
 // might run into issues without pagination!
 exports.sales = functions.https.onRequest(async (req, res) => {
-    let salesData = { salesCount: 0,openHours: 0, totals: { total: 0, tip: 0 }, byHour: [] }
+    let salesData = { salesCount: 0, openHours: 0, totals: { total: 0, tip: 0 }, byHour: [] }
     for (let i = 0; i < 23; i++)
         salesData.byHour.push({ hour: i, total: 0, tip: 0 })
     const { start, end, locationId } = req.query
-    const response = await client.ordersApi.searchOrders({
-        locationIds: [
-            locationId
-        ],
-        query: {
-            filter: {
-                dateTimeFilter: {
-                    createdAt: {
-                        startAt: start,
-                        endAt: end
-                    }
-                },
+    let cursor;
+    do {
+        const response = await client.ordersApi.searchOrders({
+            locationIds: [
+                locationId
+            ],
+            query: {
+                filter: {
+                    dateTimeFilter: {
+                        createdAt: {
+                            startAt: start,
+                            endAt: end
+                        }
+                    },
+                }
+            },
+            cursor: cursor
+        })
+        response.result.orders.forEach(order => {
+            // hacky, the time on square is 6 hours ahead of mountain standard so we subtract 6
+            const hour = new Date(order.createdAt).toISOString().slice(0, 13).split("T")[1] - 6
+            // total values are stored in cents so divide by 100
+            const total = +Math.round(order.totalMoney.amount.toString().replace("n", "") / 100)
+            const tip = +Math.round(order.totalTipMoney.amount.toString().replace("n", "") / 100)
+            const sale = { total, tip, hour }
+            salesData.totals.total += total
+            salesData.totals.tip += tip
+            //salesData.sales += sale
+            if (salesData.byHour[hour]) {
+                salesData.byHour[hour].total += total
+                salesData.byHour[hour].tip += tip
             }
-        }
-    })
-    response.result.orders.forEach(order => {
-        // hacky, the time on square is 6 hours ahead of mountain standard so we subtract 6
-        const hour = new Date(order.createdAt).toISOString().slice(0, 13).split("T")[1] - 6
-        // total values are stored in cents so divide by 100
-        const total = +Math.round(order.totalMoney.amount.toString().replace("n", "") / 100)
-        const tip = +Math.round(order.totalTipMoney.amount.toString().replace("n", "") / 100)
-        const sale = { total, tip, hour }
-        salesData.totals.total += total
-        salesData.totals.tip += tip
-        //salesData.sales += sale
-        salesData.byHour[hour].total += total
-        salesData.byHour[hour].tip += tip
-        salesData.salesCount ++;
-    })
+            salesData.salesCount++;
+        })
+        cursor = response.result.cursor
+    } while (cursor)
     salesData.byHour.forEach(hour => {
-        if(hour.total > 0)
-            salesData.openHours ++;
+        if (hour.total > 0)
+            salesData.openHours++;
     })
     res.set('Access-Control-Allow-Origin', '*');
     res.status(200).send(salesData);
